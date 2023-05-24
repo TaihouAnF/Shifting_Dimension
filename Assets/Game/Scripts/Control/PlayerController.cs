b9bf57d50a0e07cff251ad6fb93d42fed4ebd7d4
Vandereck <127145479+Vandereck@users.noreply.github.com>
2023-05-24 13:26:38 +0100

    Adjusted Jump

--- Assets/Game/Scripts/Control/PlayerController.cs
+++ Assets/Game/Scripts/Control/PlayerController.cs
@@ -29,6 +29,8 @@ public class PlayerController : MonoBehaviour
     private Animator playerAnim;    // Player Animation for running/jumping/switching
     private AudioSource playerAudi; // Player Audio when running/jumping/switching
 
+    [SerializeField] float gravity;
+
     
     // private GameManagerDependencyInfo gameManager; // A game manager for getting status of the player /* Uncomment it if we have one */
 
@@ -47,8 +49,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // movement and jump
         Move();
+
+        
 
         // Switching
         // if (Input.GetButtonDown("Fire 2"))   // Need to add one more button in the control system
@@ -76,23 +81,44 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void FreeFall()// calculate the speed of falling, modified by gravity
+    {
+        if (playerRb.velocity.y < 0)
+        {
+            playerRb.AddForce(Vector3.down * playerRb.mass * gravity, ForceMode.Force);
+        }
+    }
+
+    private bool OnTheGround()// this bools checks if we are on the ground, used to make sure we are not double jumping
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 3.5f, 1 << 8))
+        {
+
+            return true;
+        }
+        return false;
+    }
+
     private void Move()
     {
         // Movement And we want to use physics so we utilize velocity instead of translate
         float horizontalInput = Input.GetAxis("Horizontal");
         playerRb.velocity = new Vector2(horizontalInput * moveSpeed, playerRb.velocity.y);
 
-        if(Mathf.Abs(playerRb.velocity.y) <=0)
-        {
-            ontheground = true;
-        }
+        // if(Mathf.Abs(playerRb.velocity.y) <=0)
+        // {
+        //     ontheground = true;
+        // }
 
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && ontheground == true)
+        if (Input.GetKeyDown(KeyCode.Space) && ontheground == true &&OnTheGround())
         {
             print("jump");
             Jumping();
         }
+
+        FreeFall(); 
 
     //     if (IsGrounded() && Mathf.Abs(playerRb2D.velocity.x) > speedThreshold && !playerAudi.isPlaying)
     //     {
@@ -113,17 +139,26 @@ public class PlayerController : MonoBehaviour
 
     private void Jumping()
     {
+        audioManager.PlayJumpAudio();
         playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
         ontheground = false;
+        StartCoroutine(ResetJumping());
+
         // if (playerAudi.isPlaying) // Make sure the audio stops when jumping
         // {
         //     playerAudi.Stop();
         // }
         // playerAnim.SetTrigger("Jump_trigger"); /* Uncomment this if we find running animation */
 
-        audioManager.PlayJumpAudio();
         
         
+        
+    }
+
+    IEnumerator ResetJumping()// this IeNumerator will make sure that when we jumpo we don't risk to have a second jumped stored cause of the the Update method
+    {
+        yield return new WaitForSeconds(1f);
+        ontheground = true;
     }
 
    
