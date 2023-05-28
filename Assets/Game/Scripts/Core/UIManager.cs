fdffb1f081680bec93b3c7cc0cb26a80aed63391
Vandereck <127145479+Vandereck@users.noreply.github.com>
2023-05-28 15:40:53 +0100

    Game Over/Lives system

--- Assets/Game/Scripts/Core/UIManager.cs
+++ Assets/Game/Scripts/Core/UIManager.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+  public Image[] healthImage;
   public void LoadScene1()
   {
     SceneManager.LoadScene(1);
   }
+
+  public void UpdateLives(int livesremaining)
+  {
+    for (int i = 0; i <= livesremaining; i ++)
+    {
+      if(i == livesremaining)
+      {
+        healthImage[i].enabled = false;
+      }
+    }
+  }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
