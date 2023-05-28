8a93c99e67e8600cc815773d49d72aa7e9a44852
Taihou F <59370291+AnsonFeng1999@users.noreply.github.com>
2023-05-28 14:17:00 -0400

    Refactored code.

--- Assets/Game/Scripts/Core/UIManager.cs
+++ Assets/Game/Scripts/Core/UIManager.cs
@@ -7,22 +7,23 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
   public Image[] healthImage;
-  public void LoadScene1()
-  {
-    SceneManager.LoadScene(1);
-  }
 
-  public void UpdateLives(int livesremaining)
-  {
-    for (int i = 0; i <= livesremaining; i ++)
+
+    public void UpdateLives(int livesremaining)
     {
-      if(i == livesremaining)
-      {
-        healthImage[i].enabled = false;
-      }
+        for (int i = 0; i <= livesremaining; i++)
+        {
+            if (i == livesremaining)
+            {
+                healthImage[i].enabled = false;
+            }
+        }
     }
-  }
 
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene(1);
+    }
     public void LoadScene3()
     {
         SceneManager.LoadScene(3);
